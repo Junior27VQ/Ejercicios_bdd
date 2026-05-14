119220b47e5ef8de99904de3ea3e7c439b1f4274
Junior27VQ <favq1999@outlook.com>
2026-05-14 18:56:27 -0500

    create student table

--- estudiantes.sql
+++ estudiantes.sql
@@ -1,1 +1,9 @@
 --Crear la Tabla
+create table estudiantes(
+	id_estudiante int not null primary key,
+	nombres varchar(50) not null,
+	apellidos varchar(50) not null,
+	edad int not null,
+	curso varchar(50) not null,
+	fecha_registro varchar(10) not null
+);
