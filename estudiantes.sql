6a86abe5e022b7a2efe3239368576c4206323841
Junior27VQ <favq1999@outlook.com>
2026-05-14 19:26:10 -0500

    Update Data

--- estudiantes.sql
+++ estudiantes.sql
@@ -23,3 +23,10 @@ select * from estudiantes where edad>18 and edad<25;
 select * from estudiantes where curso='Base de Datos';
 select * from estudiantes where fecha_registro>'2026-03-01';
 select * from estudiantes where fecha_registro between '2026-01-01' and '2026-04-30';
+
+--UPDATE Actualizar Datos
+update estudiantes set curso='1B' where id_estudiante=1;
+update estudiantes set edad=18 where curso='1B';
+update estudiantes set fecha_registro='2026-05-25' where id_estudiante=3;
+update estudiantes set curso='2B', nombres='Samira' where id_estudiante=4;
+update estudiantes set apellidos='Requejo' where id_estudiante=5;
